69b1929ec5098d43c8f35780cd3a969b7a77be8d
Eric Rodriguez <feric.rodriguez@gmail.com>
2023-06-04 21:39:55 -0300

    input affiliate form success styles changed

--- src/components/AffiliateMessage.jsx
+++ src/components/AffiliateMessage.jsx
@@ -7,18 +7,26 @@ export const AffiliateMessage = ({ success, user }) => {
 		fail: 'bg-red-200 text-red-800 border-red-600'
 	}
 
+	const SucessCard = ({dni, name, surname}) => (
+		<>
+			<h3 className='font-bold text-2xl text-center mb-2'>Usuario</h3>
+			<p className='font-bold text-lg'>DNI: {dni}</p>
+			<p className='font-bold text-lg'>Nombre: {name}</p>
+			<p className='font-bold text-lg'>Apellido: {surname}</p>
+		</>
+	);
+
 	return (
 		<div
 			className="w-full h-auto flex justify-center"
 		>
-			<div className={`w-11/12 min-h-[100px] max-w-[800px] border-2 rounded-lg flex items-center justify-center ${success && user.dni ? styles.success : styles.fail}`}>
-				<p className={`font-bold text-2xl`}>
+			<div className={`w-11/12 min-h-[100px] max-w-[800px] border-2 rounded-lg flex flex-col py-2 gap-1 items-center justify-center ${success && user.dni ? styles.success : styles.fail}`}>
+
 					{
-						success && user.dni
-						? `Usuario: DNI № ${user?.dni}`
-						: 'Usuario no encontrado'
+						success && user
+						? <SucessCard {...user}/>
+						: <h3 className='font-bold text-2xl text-center'>Usuario no encontrado</h3>
 					}
-				</p>
 			</div>
 		</div>
 	)
